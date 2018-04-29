6f868c7e281486b0c8958f1c077cc51d2b6ec0fe
JmmCz <32850669+JmmCz@users.noreply.github.com>
2018-04-29 21:31:02 +0200

    Update gauss_method.F90
    Gauss method worked fine but it has to be changed a little bit because of our matrix in main program, which is N - 1 x N size. We have here N, and in main program we are using gauss(..., N-1) so we have to add extra 1 to i in columns + start with j equals 0 (adding extra one as well everywhere) to have "N" (not N-1) columns. Moreover, we do not need main program created here as an example anymore, so I will make it as a comment. We also do not need extra computing for A matrix (also marked as an comment).

--- gauss_method.F90
+++ gauss_method.F90
@@ -12,18 +12,19 @@ module gauss_method
         real (kind = 4) :: c
 
         do i=1, N
-            do j=1, N
+            do j=0, N
                 if(i .NE. j) then ! i!=j
-                    c = A(i, j) / A(i, i)
-                    A(:, j) = A(:, j) - c * A(:, i)
-                    X(j) = X(j) - c * X(i)
-                    
-                    do i=1, N
-                       c = 1 / A(i, i)
-                       A(i, i) = 1
-                       X(i) = c * X(i) 
-                    end do 
-           
+                    c = A(i, j+1) / A(i, i+1)
+                    A(:, j+1) = A(:, j+1) - c * A(:, i+1)
+                    X(j+1) = X(j+1) - c * X(i+1)
+ 
+ ! extra computing out:
+ !                  do i=1, N
+ !                      c = 1 / A(i, i)
+ !                      A(i, i) = 1
+ !                      X(i) = c * X(i) 
+ !                   end do 
+ !          
                 end if
             end do
         end do
@@ -32,36 +33,36 @@ module gauss_method
 
 end module
 
-program main
-
-    use gauss_method, only: gauss
-
-    integer (kind = 4) :: N, i, j
-    real (kind = 4), allocatable :: A(:,:), X(:)
-
-    read(*, *) N
-
-    allocate (A(N, N))
-    allocate (X(N))
-
-    do i=1, N
-        do j=1, N
-            read(*, *) A(j, i)
-        end do
-    end do
-
-    write(*, *) A
-
-    do i=1, N
-        read(*,*) X(i)
-    end do
-
-    call gauss(A, X, N)
-
-    write(*, *) X
-
-    deallocate (A)
-    deallocate (X)
-
-end program
-
+!program main
+!
+!    use gauss_method, only: gauss
+!
+!    integer (kind = 4) :: N, i, j
+!    real (kind = 4), allocatable :: A(:,:), X(:)
+!
+!    read(*, *) N
+!
+!    allocate (A(N, N))
+!    allocate (X(N))
+!
+!    do i=1, N
+!        do j=1, N
+!            read(*, *) A(j, i)
+!        end do
+!    end do
+!
+!    write(*, *) A
+!
+!    do i=1, N
+!        read(*,*) X(i)
+!    end do
+!
+!    call gauss(A, X, N)
+!
+!    write(*, *) X
+!
+!    deallocate (A)
+!    deallocate (X)
+!
+!end program
+!
