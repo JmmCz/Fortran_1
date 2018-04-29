2a011bc290b17b2477c299a108faff2f157b3535
JmmCz <32850669+JmmCz@users.noreply.github.com>
2018-04-29 21:07:34 +0200

    Update gauss_method.F90
    Improving Gauss so that it corrects A(N, N) array +  creating simple main program that gets our input in order to check Gauss correctness. It works fine :)

--- gauss_method.F90
+++ gauss_method.F90
@@ -17,6 +17,13 @@ module gauss_method
                     c = A(i, j) / A(i, i)
                     A(:, j) = A(:, j) - c * A(:, i)
                     X(j) = X(j) - c * X(i)
+                    
+                    do i=1, N
+                       c = 1 / A(i, i)
+                       A(i, i) = 1
+                       X(i) = c * X(i) 
+                    end do 
+           
                 end if
             end do
         end do
@@ -24,3 +31,37 @@ module gauss_method
     end subroutine
 
 end module
+
+program main
+
+    use gauss_method, only: gauss
+
+    integer (kind = 4) :: N, i, j
+    real (kind = 4), allocatable :: A(:,:), X(:)
+
+    read(*, *) N
+
+    allocate (A(N, N))
+    allocate (X(N))
+
+    do i=1, N
+        do j=1, N
+            read(*, *) A(j, i)
+        end do
+    end do
+
+    write(*, *) A
+
+    do i=1, N
+        read(*,*) X(i)
+    end do
+
+    call gauss(A, X, N)
+
+    write(*, *) X
+
+    deallocate (A)
+    deallocate (X)
+
+end program
+
